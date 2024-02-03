596fdd7166531ea791a2c8d014adc8532dfd6b11
GreyShadow46 <callumchavez89@gmail.com>
2024-02-03 23:54:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -307,8 +307,9 @@ app.post('/signin', (req, res) => {
       if(account.length !== 0){
         if(account[0].bannedTime !== ''){
           console.log(account)
+          console.log(account[0].bannedTime + (1 * 24 * 60 * 60 * 1000))
+          console.log(new Date().getTime())
           if((account[0].bannedTime + (1 * 24 * 60 * 60 * 1000)) <= new Date().getTime()){
-            console.log(account)
             con.query("UPDATE accounts SET attempts = 0, bannedTime = '' WHERE userName = '" + req.body.username + "'", function (err, result, fields) {
               if(err) throw err
               console.log("1 record updated");
